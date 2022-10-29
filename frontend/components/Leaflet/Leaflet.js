86a20397d139befac5f07cec9b8eba3395ce22c0
Michal Vaniš <zirixcz@gmail.com>
2022-10-29 20:17:19 +0200

    feat: added db for celltowers
    Displaying markers depending on the data in the database

--- frontend/components/Leaflet/Leaflet.js
+++ frontend/components/Leaflet/Leaflet.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {ExpoLeaflet} from "expo-leaflet";
 import * as Location from 'expo-location';
 import {View} from "react-native";
+import {celltowers} from "../../db/celltowers";
 
 const Leaflet = () => {
 
@@ -38,18 +39,17 @@ const Leaflet = () => {
         },
     ]
 
-    const mapMarkers = [
-        {
-            id: "0",
+    const mapMarkers = celltowers.map(tower => {
+        return {
             position: {
-                lat: 50.428872222222,
-                lng: 15.582430555556,
+                lat: tower.lat,
+                lng: tower.lng
             },
             icon: "https://www.pinclipart.com/picdir/middle/537-5374089_react-js-logo-clipart.png",
             size: [32, 32],
-        },
-    ]
-
+        }
+    })
+    
     return (
         <View style={{backgroundColor: "black", height: "100%"}}>
             <ExpoLeaflet
