781b25fdb09fde1c79a102e8a518c9bcc0318a78
Michal Vaniš <zirixcz@gmail.com>
2022-10-30 01:04:42 +0200

    feat: providers can now be closed

--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -12,19 +12,19 @@ const Navbar = (props) => {
     return (
         <View style={styles.container}>
             <Pressable style={provider === 1 ? {backgroundColor: '#cecc', borderRadius: 5} : null}
-                       onPress={() => setProvider(1)}>
+                       onPress={() => setProvider(provider === 1 ? -1 : 1)}>
                 <Text>O2</Text>
             </Pressable>
             <Pressable style={provider === 2 ? {backgroundColor: '#cecc', borderRadius: 5} : null}
-                       onPress={() => setProvider(2)}>
+                       onPress={() =>  setProvider(provider === 2 ? -1 : 2)}>
                 <Text>tmobile</Text>
             </Pressable>
             <Pressable style={provider === 3 ? {backgroundColor: '#cecc', borderRadius: 5} : null}
-                       onPress={() => setProvider(3)}>
+                       onPress={() =>  setProvider(provider === 3 ? -1 : 3)}>
                 <Text>vodafone</Text>
             </Pressable>
             <Pressable style={provider === 4 ? {backgroundColor: '#cecc', borderRadius: 5} : null}
-                       onPress={() => setProvider(4)}>
+                       onPress={() =>  setProvider(provider === 4 ? -1 : 4)}>
                 <Text>poda</Text>
             </Pressable>
         </View>
