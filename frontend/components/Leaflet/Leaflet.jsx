e1eafda317d3890b867bacb3ba3477b263bf5196
IAmJustSomeRandomGuy <ireallyamjustsomerandomguy@gmail.com>
2022-10-30 13:20:41 +0100

    changed linear search to half binary

--- frontend/components/Leaflet/Leaflet.jsx
+++ frontend/components/Leaflet/Leaflet.jsx
@@ -42,20 +42,74 @@ const Leaflet = () => {
         },
     ]
 
+    // Does a binary search of the celltower array
+    function biSearch(array, condition, start = 0, end = array.length - 1, ...funcParameters) {
+        let mid = Math.floor((start + end) / 2);
+        // condition should return 0 if condition is true 1 if the mid is bigger -1 if the mid is smaller
+        switch (condition(array[mid].lat, ...funcParameters)) {
+            case 0:
+                return mid;
+            case 1:
+                return biSearch(array, condition, start, mid + 1);
+            case -1:
+                return biSearch(array, condition, mid - 1, end);
+        }
+        throw Error("condition output is not -1, 0 or 1!!!");
+    }
+    // Slices array so that only the towers within the lat offset are left
+    function linearSearchSubArray(array, index, offset){
+        let result = [];
+        // looks at smaller indexes if they still count
+        let temp = index;
+        while (result.length === 0) {
+            temp--;
+            if (array[temp].lat < userCurrentLocation.coords.latitude - offset) result.push(temp + 1);
+            if (array[temp] === undefined) throw Error("Index out of range of array");
+        }
+        // looks at bigger indexes if they still count
+        temp = index;
+        while (result.length === 1) {
+            temp++;
+            if (array[temp].lat > userCurrentLocation.coords.latitude + offset) result.push(temp - 1);
+            if (array[temp] === undefined) throw Error("Index out of range of array");
+        }
+        return array.slice(...result);
+    }
+    // makes an array of towers with chosen operator
+    let celltowersOperator = Array.from(celltowers).filter((tower) =>
+        tower.operators.includes(provider === 1 ? "o2" : provider === 2 ? "tmobile" : provider === 3 ? "vodafone" : provider === 4 ? "poda" : null)
+    );
+    // searches the array to ectract only the ones near the user
+    let celltowersNearBy = []
+    if (celltowersOperator.length > 0) {
+        let location = biSearch(
+            celltowersOperator,
+            (lat, offset) => {
+                // returns 1 if the towers lat is bigger then users position + offset, -1 when smaller and 0 if its within
+                return lat > userCurrentLocation.coords.latitude + offset ? 1 : lat < userCurrentLocation.coords.latitude - offset ? -1 : 0;
+            },
+            0,
+            celltowersOperator.length - 1,
+            0.1
+        );
+
+        celltowersNearBy = linearSearchSubArray(celltowersOperator, location, 0.1).filter((tower) => {return userCurrentLocation.coords.longitude + 0.1 > tower.lng && userCurrentLocation.coords.longitude - 0.1 < tower.lng; });
+    }
+
+
     useEffect(() => {
-        setMapMarkers(Array.from(celltowers)
-            .filter(tower => tower.operators.includes(provider === 1 ? "o2" : provider === 2 ? "tmobile" : provider === 3 ? "vodafone" : provider === 4 ? "poda" : null))
-            .filter(tower => userCurrentLocation.coords.longitude + 0.1 > tower.lng && userCurrentLocation.coords.longitude - 0.1 < tower.lng && userCurrentLocation.coords.latitude + 0.1 > tower.lat && userCurrentLocation.coords.latitude - 0.1 < tower.lat)
-            .map(tower => {
+        setMapMarkers(
+            celltowersNearBy.map((tower) => {
                 return {
                     position: {
                         lat: tower.lat,
-                        lng: tower.lng
+                        lng: tower.lng,
                     },
-                    icon: '<span>📡</span>',
+                    icon: "<span>📡</span>",
                     size: [32, 32],
-                }
-            }))
+                };
+            })
+        );
     }, [provider])
 
     return (
