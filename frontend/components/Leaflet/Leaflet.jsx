88dd35425dbe252f2010cd6be9f9943d4318d18f
Michal Vaniš <zirixcz@gmail.com>
2022-11-04 21:49:35 +0100

    update: improved performance on older devices

--- frontend/components/Leaflet/Leaflet.jsx
+++ frontend/components/Leaflet/Leaflet.jsx
@@ -48,10 +48,6 @@ const Leaflet = () => {
     }, []);
 
     useEffect(() => {
-        console.log(threeAxisData)
-    }, [threeAxisData])
-
-    useEffect(() => {
 
         const getCurrentPosition = async () => {
             let {status} = await Location.requestForegroundPermissionsAsync();
@@ -66,10 +62,6 @@ const Leaflet = () => {
 
     }, []);
 
-    useEffect(() => {
-        console.log(userCurrentLocation)
-    }, [userCurrentLocation])
-
     const mapLayers = [
         {
             attribution: '&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
@@ -79,96 +71,20 @@ const Leaflet = () => {
         },
     ]
 
-    // Does a binary search of the celltower array
-    function biSearch(array, condition, start = 0, end = array.length - 1, ...funcParameters) {
-        let mid = Math.floor((start + end) / 2);
-        // condition should return 0 if condition is true 1 if the mid is bigger -1 if the mid is smaller
-        switch (condition(array[mid].lat, ...funcParameters)) {
-            case 0:
-                return mid;
-            case 1:
-                return biSearch(array, condition, start, mid + 1, ...funcParameters);
-            case -1:
-                return biSearch(array, condition, mid - 1, end, ...funcParameters);
-        }
-        throw Error("condition output is not -1, 0 or 1!!!");
-    }
-
-    // Slices array so that only the towers within the lat offset are left
-    function linearSearchSubArray(array, index, offset) {
-        let result = [];
-        // looks at smaller indexes if they still count
-        let temp = index;
-        while (result.length === 0) {
-            temp--;
-            if (array[temp].lat < userCurrentLocation.coords.latitude - offset) result.push(temp + 1);
-            if (array[temp] === undefined) result.push(temp + 1);
-        }
-        // looks at bigger indexes if they still count
-        temp = index;
-        while (result.length === 1) {
-            temp++;
-            if (array[temp].lat > userCurrentLocation.coords.latitude + offset) result.push(temp - 1);
-            if (array[temp] === undefined) result.push(temp - 1);
-        }
-        return array.slice(...result);
-    }
-
-
-    // the ofset of how far from user it should look
-    const offset = 0.1;
-    // multiplies longitude with this to change ratio of sides
-    const offsetShape = 1.6;
-
-    // makes an array of towers with chosen operator
-    const celltowersOperator = Array.from(celltowers).filter((tower) =>
-        tower.operators.includes(provider === 1 ? "o2" : provider === 2 ? "tmobile" : provider === 3 ? "vodafone" : provider === 4 ? "poda" : null)
-    );
-    // searches the array to ectract only the ones near the user
-    let celltowersNearBy = [];
-    if (celltowersOperator.length > 0) {
-        const location = biSearch(
-            celltowersOperator,
-            (lat, offset) => {
-                // returns 1 if the towers lat is bigger then users position + offset, -1 when smaller and 0 if its within
-                return lat > userCurrentLocation.coords.latitude + offset ? 1 : lat < userCurrentLocation.coords.latitude - offset ? -1 : 0;
-            },
-            0,
-            celltowersOperator.length - 1,
-            offset
-        );
-
-        celltowersNearBy = linearSearchSubArray(celltowersOperator, location, offset).filter((tower) => {
-            return (
-                userCurrentLocation.coords.longitude + offset * offsetShape > tower.lng &&
-                userCurrentLocation.coords.longitude - offset * offsetShape < tower.lng
-            );
-        });
-    }
-
-
     useEffect(() => {
-        setMapMarkers(
-            celltowersNearBy
-                .map((tower) => {
-                    return {
-                        position: {
-                            lat: tower.lat,
-                            lng: tower.lng,
-                        },
-                        icon: "<span>📡</span>",
-                        size: [32, 32],
-                    };
-                })
-                .concat({
+        setMapMarkers(Array.from(celltowers)
+            .filter(tower => tower.operators.includes(provider === 1 ? "o2" : provider === 2 ? "tmobile" : provider === 3 ? "vodafone" : provider === 4 ? "poda" : null))
+            .filter(tower => userCurrentLocation.coords.longitude + 0.16 > tower.lng && userCurrentLocation.coords.longitude - 0.16 < tower.lng && userCurrentLocation.coords.latitude + 0.1 > tower.lat && userCurrentLocation.coords.latitude - 0.1 < tower.lat)
+            .map(tower => {
+                return {
                     position: {
-                        lat: userCurrentLocation.coords.latitude,
-                        lng: userCurrentLocation.coords.longitude,
+                        lat: tower.lat,
+                        lng: tower.lng
                     },
-                    icon: "<span>🔴</span>",
-                    size: [16, 16],
-                })
-        );
+                    icon: '<span>📡</span>',
+                    size: [32, 32],
+                }
+            }))
     }, [provider])
 
     return (
