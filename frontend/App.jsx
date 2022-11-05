586a793d1aab955aa99192140896789f673a10b6
Pero-coder <67205912+Pero-coder@users.noreply.github.com>
2022-11-05 19:55:55 +0100

    welcome page

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -2,6 +2,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Leaflet from "./components/Leaflet/Leaflet";
 import Compass from "./components/Compass/Compass";
+import Welcome from './components/Welcome/Welcome';
 
 export default function App() {
 
@@ -10,6 +11,7 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen name="Wellcome" component={Welcome} options={{headerShown: false}}/>
                 <Stack.Screen name="Map" component={Leaflet} options={{headerShown: false}}/>
                 <Stack.Screen name="Compass" component={Compass} options={{headerShown: false}}/>
             </Stack.Navigator>
