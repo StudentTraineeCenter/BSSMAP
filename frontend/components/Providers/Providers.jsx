ef0620f6d0afbff44fe52fed59166d064400ecba
Michal Vaniš <zirixcz@gmail.com>
2023-01-29 16:53:45 +0100

    handle leaflet rendering distance

--- frontend/components/Providers/Providers.jsx
+++ frontend/components/Providers/Providers.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const styles = StyleSheet.create({
@@ -15,7 +16,7 @@ const styles = StyleSheet.create({
     width: "70%",
     shadowColor: "black",
     shadowOpacity: 0.9,
-    elevation: 4
+    elevation: 4,
   },
   text: {
     fontSize: 20,
@@ -24,6 +25,17 @@ const styles = StyleSheet.create({
 });
 
 const Providers = ({ navigation }) => {
+  const [distance, setDistance] = useState(0.1);
+
+  const handleDistance = () => {
+    if (distance < 300) {
+      setDistance(distance*2)
+      return
+    }
+
+    setDistance(0.1)
+  };
+
   return (
     <View style={styles.container}>
       <Text
@@ -31,7 +43,7 @@ const Providers = ({ navigation }) => {
           fontSize: 40,
           fontFamily: "bold",
           color: "#5263A0",
-          marginBottom: 30
+          marginBottom: 30,
         }}
       >
         Operátoři
@@ -39,26 +51,33 @@ const Providers = ({ navigation }) => {
 
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate("Map", { provider: 1 })}
+        onPress={() => navigation.navigate("Map", { provider: 1, distance: distance })}
       >
         <Text style={styles.text}>O2</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate("Map", { provider: 2 })}
+        onPress={() => navigation.navigate("Map", { provider: 2, distance: distance })}
       >
         <Text style={styles.text}>T-Mobile</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate("Map", { provider: 3 })}
+        onPress={() => navigation.navigate("Map", { provider: 3, distance: distance })}
       >
         <Text style={styles.text}>Vodafone</Text>
       </TouchableOpacity>
 
-      <Text style={{ color: "#5263A0", fontSize: 15, margin: 30 }}>
+      <Text style={{textAlign: "left"}}>
+        Vzdálenost vykreslování
+      </Text>
+      <TouchableOpacity style={styles.button} onPress={handleDistance}>
+        <Text style={styles.text}>{distance}km</Text>
+      </TouchableOpacity>
+
+      <Text style={{ color: "#5263A0", fontSize: 15, margin: 15 }}>
         Někteří operátoři využívají sítě jiných operátorů, pokud nevíte, kterou
         síť váš operátor využívá, kontaktujte ho.
       </Text>
